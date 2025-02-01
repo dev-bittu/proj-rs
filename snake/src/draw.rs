947ef753028d1b80a1b3e23e86a0a631a013bb13
bittu <thedevbittu@gmail.com>
2025-02-01 09:13:25 +0530

    hello world

--- snake/src/draw.rs
+++
@@ -1,2 +0,0 @@
-use piston_window::{rectangle, Context, G2d};
-use piston_window::types::Color;
--- snake/src/main.rs
+++ snake/src/main.rs
@@ -1,6 +1,3 @@
-extern crate rand;
-extern crate piston_window;
-
 fn main() {
     println!("Hello, world!");
 }
